1903494e39711b7f490bfc85156ac462338566b7
Chris Schneider <Schmed@TransPac.com>
2015-01-22 20:51:32 -0800

    Tailor default (no-check) SCT to Ken's class

--- bitney_adventure/always_fails_sct.py
+++ bitney_adventure/always_fails_sct.py
@@ -1,1 +1,16 @@
-return "The default Submission Correctness Test always fails."
+if (error):
+    return """You broke the code with your changes so that it is
+no longer valid Python syntax.  The cryptic error message to the
+right will identify the first line that Python didn't like.
+You can try to fix the error you introduced, or just click the
+Reset Code button and start again."""
+
+if (not ('is_period_over' in globals())):
+    return '''(Not really oops.) Python didn't seem to have any
+problem the code you wrote, but that doesn't necessarily mean
+that it's doing what it was intended to do.  Have another look
+over the description of what it's supposed to do.
+If you're confident about your code, call Mr. Krugler over to
+have a look.'''
+
+return True
