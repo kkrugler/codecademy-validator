d8706df95ded8fdfe1ea7d672e71b3b99ad86c56
Chris Schneider <Schmed@TransPac.com>
2015-02-11 10:00:54 -0800

    Copy SCT 3.1 bug fix to 4.3 (needed to check for output case-insensitively).

--- intro_to_cs/section4/section4_exercise3_sct.py
+++ intro_to_cs/section4/section4_exercise3_sct.py
@@ -145,22 +145,26 @@ statements to the program. Click the Reset Code button and start over.'''
 
 user_printed_line = None
 for line in printed_lines[printed_line_count:]:
-    if (line.strip().startswith('More than')):
+    if (line.strip().lower().startswith('more than')):
         return '''The code you added did end up printing "%s",
-but its condition should not have been True.''' % line
-    if (line.strip().startswith('Fewer than')):
+but its condition should not have been true.''' % line
+    elif (line.strip().lower().startswith('fewer than')):
         if (user_printed_line):
             return '''The code you added ended up printing
 at least two lines that began with "Fewer than".'''
         else:
             user_printed_line = line
-    elif (len(printed_lines) > (printed_line_count + 1)):
+    elif (len(printed_lines) > (printed_line_count)):
         return '''The code you added printed the unexpected line "%s"''' % line
 
-result = check_text('Fewer than ten lines were printed (not including this one)',
-                    user_printed_line,
-                    'printed line')
-if (result != True):
-    return result
+if (user_printed_line):
+    result = check_text('Fewer than ten lines were printed (not including this one)',
+                        user_printed_line,
+                        'printed line')
+    if (result != True):
+        return result
+else:
+    return '''The code you added didn't print anything that
+starts with "Fewer than".'''
 
 return True
